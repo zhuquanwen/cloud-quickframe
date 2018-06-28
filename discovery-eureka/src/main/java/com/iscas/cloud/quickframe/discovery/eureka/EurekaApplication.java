4b911849cf9a0feb75658a78615459a7042afc99
zhuquanwen <76775081@qq.com>
2018-06-28 14:13:33 +0800

    eureka配置logback

--- discovery-eureka/src/main/java/com/iscas/cloud/quickframe/discovery/eureka/EurekaApplication.java
+++ discovery-eureka/src/main/java/com/iscas/cloud/quickframe/discovery/eureka/EurekaApplication.java
@@ -17,6 +17,8 @@
 
 package com.iscas.cloud.quickframe.discovery.eureka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -27,7 +29,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
+	private static Logger LOGGER = LoggerFactory.getLogger(EurekaApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
+		LOGGER.info("eureka启动");
 	}
 }
